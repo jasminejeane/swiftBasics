97b794105c653ea3723c1f16b0ab133cff7158f8
jasminejeane <jasminejeane@gmail.com>
2017-02-19 12:19:44 -0800

    added new page

---
+++ SwiftBasics/SwiftBasics.playground/Pages/Types.xcplaygroundpage/Contents.swift
@@ -0,0 +1,43 @@
+// String Literal
+
+let country = "United States of America"
+
+let state = "California"
+
+let city = "San Pablo"
+
+let address = country + "" + state + ", " + city
+
+//can not concate a street and a number
+
+let interpolate = "\(country), \(state), \(city)"
+
+/*
+ ________________
+ INTEGERS
+ ________________
+ 
+ */
+
+let favLang = "Swift"
+
+let year = 2014
+
+
+/*
+ ________________
+FLOATING PT. NUMBERS
+ ________________
+ 
+ */
+
+let version = 3.1
+
+/*
+ ________________
+ BOOLEAN
+ ________________
+ 
+ */
+
+
